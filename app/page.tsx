594829b10071b80e8c12af14b6fce4df03deac20
Christopher <christopher.fong115@gmail.com>
2025-01-24 04:49:43 -0500

    comments and black strike bar with comment

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,39 @@ import FeedbackCard from "@/components/FeedbackCard";
 import Navbar from "@/components/Navbar";
 import { IoMdCheckmark } from "react-icons/io";
 
+const webHavest = [
+  {
+    icon: "/icon01.svg",
+    title: "1. General Web Scraping",
+    text: "ScrapingBee web scraping API works great for general web scraping tasks like real estate scraping, price-monitoring, extracting reviews without getting blocked.",
+  },
+  {
+    icon: "/data.svg",
+    title: "2. Data Extraction",
+    text: "Getting HTML is cool, getting formatted JSON data is better. Thanks to our easy-to-use extraction rules, get just the data you need with one simple API call.",
+  },
+  {
+    icon: "/js.svg",
+    title: "3. JavaScript scenario",
+    text: "If you need to click, scroll, wait for some elements to appear or just run some custom JavaScript code on the website you want to scrape, check our JS scenario feature.",
+  },
+  {
+    icon: "/picture-1.svg",
+    title: "4. Screenshots",
+    text: "Need a screenshot of that website and not HTML? You can do this very easily with our screenshot feature. We also support full page and partial screenshots!",
+  },
+  {
+    icon: "/search.svg",
+    title: "5. Search Engine Result Page",
+    text: "Scraping search engine result pages is extremely painful because of rate limits. Thanks to our Google search API, it's now easier than ever.",
+  },
+  {
+    icon: "/nocode.svg",
+    title: "6. No code web scraping",
+    text: "If coding is not your thing, you can leverage our Make integration to create custom web scraping engines without writing a single line of code!",
+  },
+];
+
 export default function Home() {
   return (
     <>
@@ -23,7 +56,7 @@ export default function Home() {
         </div>
         <img src="/hero_illustration.svg" />
       </div>
-      <div className="flex items-center w-2/3 gap-20 justify-center mx-auto mt-[20%]">
+      <div className="flex items-center w-7/12 gap-20 justify-center mx-auto mt-[20%]">
         <img src="/feature_headless.svg" />
         <div>
           <h1 className="text-5xl mb-10 font-bold">
@@ -56,7 +89,7 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="flex items-center w-2/3 gap-20 justify-center mx-auto mt-[20%]">
+      <div className="flex items-center w-7/12 gap-20 justify-center mx-auto mt-[20%]">
         <div>
           <h1 className="text-5xl mb-10 font-bold">
             Render JavaScript to scrape{" "}
@@ -90,6 +123,104 @@ export default function Home() {
           />
         </div>
         <img src="/feature_rendering.svg" />
+      </div>
+      <div className="flex items-center w-7/12 gap-20 justify-center mx-auto mt-[20%]">
+        <img src="/feature_proxies.svg" />
+        <div>
+          <h1 className="text-5xl mb-10 font-bold">
+            Rotate proxies to{" "}
+            <strong className="underline decoration-yellow-400 font-bold">
+              bypass rate limiting
+            </strong>
+            .
+          </h1>
+          <p>
+            Thanks to our large proxy pool, you can bypass rate limiting while
+            scraping web pages, hiding your bots and reducing the chances of
+            being blocked.
+          </p>
+          <ul className="flex gap-10 mt-10 mb-10">
+            <li className="flex gap-2 items-center">
+              <IoMdCheckmark className="text-green-500" /> Large proxy pool
+            </li>
+            <li className="flex gap-2 items-center">
+              <IoMdCheckmark className="text-green-500" /> IP geolocation
+            </li>
+            <li className="flex gap-2 items-center">
+              <IoMdCheckmark className="text-green-500" /> Automatic proxy
+              rotation
+            </li>
+          </ul>
+          <FeedbackCard
+            companyLink="https://codesubmit.io"
+            companyName="CodeSubmit"
+            name="Dominic Phillips"
+            pfp="/dominic.jpeg"
+            position="Co-Founder"
+            text="ScrapingBee clear documentation, easy-to-use API, and great success rate made it a no-brainer."
+          />
+        </div>
+      </div>
+      <div className="text-center items-center w-7/12 gap-20 justify-center mx-auto mt-[20%]">
+        <h1 className="text-4xl font-bold mb-10">
+          Six ways to use ScrapingBee for web harvesting
+        </h1>
+        <h2 className="text-gray-500 text-2xl">
+          Wondering how our customers use our web scraping API?
+        </h2>
+        <h2 className="text-gray-500 text-2xl">
+          From a general web scrape to JavaScript rendering, our simple API does
+          it all.
+        </h2>
+        <ul className="grid grid-cols-3 mt-20 gap-10">
+          {webHavest.map((item) => (
+            <li key={item.title}>
+              <img className="aspect-square w-20 mx-auto" src={item.icon} />
+              <h2 className="font-bold text-xl py-5">{item.title}</h2>
+              <p>{item.text}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="relative mt-[20%] h-screen">
+        <div className="absolute bg-black w-[200%] h-3/4 rotate-6 -translate-x-32 -z-50" />
+        <div className="text-white w-7/12 items-center justify-center mx-auto translate-y-20">
+          <div className="rounded-2xl w-fit h-fit p-6">
+            <p className="text-4xl">
+              "Scrapingbee helps us to retrieve information from sites that use
+              very sophisticated mechanism to block unwanted traffic, we were
+              struggling with those sites for soem time now and{" "}
+              <strong>
+                I'm very glad that we found ScrapingBee.<strong>"</strong>
+              </strong>
+              "
+            </p>
+            <div className="flex gap-5 items-center mt-5">
+              <img
+                className="aspect-square rounded-2xl h-20"
+                src="/anton.jpeg"
+              />
+              <div className="text-yellow-400">
+                <h2 className="font-bold text-xl">Anton R</h2>
+                <div className="flex gap-1">
+                  <h3>CTO</h3>
+                  <a
+                    className="font-bold underline"
+                    href={
+                      "https://www.capterra.com/p/195060/ScrapingBee/reviews/Capterra___3145309/"
+                    }
+                  >
+                    (see it on Capterra)
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-center items-center w-7/12 gap-20 justify-center mx-auto mt-[20%] mb-4">
+        <h2 className="text-5xl font-bold">Simple, trnasparent pricing.</h2>
+        <h3 className="text-xl mb-20">cancel anytime, no questions asked!</h3>
       </div>
     </>
   );
